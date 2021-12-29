c21cd5e8859d08095d945005ff68d4653cf8dc38
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-12-30 01:04:31 +0530

    Added dist folder

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,5 @@ module.exports = {
   configureWebpack: {
     // lets debugger map the code within a compressed file back to its position in the original file
     devtool: "source-map",
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.(png|jpe?g|gif)$/i,
-    //       use: [
-    //         {
-    //           loader: 'file-loader',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
   },
 };
