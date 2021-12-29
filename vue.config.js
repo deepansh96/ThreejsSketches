ef5c65cf281f9734800e5a3e96b740b208fcda2c
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-12-30 01:26:43 +0530

    trying out

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicPath: "/",
-  configureWebpack: {
-    // lets debugger map the code within a compressed file back to its position in the original file
-    devtool: "source-map",
-  },
-};
